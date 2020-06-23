dc64da1394a6770eaa637d2fb13effc5ceba0ffe
Catheryne Small <catheryn.small@gmail.com>
2020-06-23 00:27:40 +0000

    finish setup

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source "https://rubygems.org"
 
-# Specify your gem's dependencies in anime_finder.gemspec
-gemspec
+
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,7 @@
-require "anime_finder/version"
+require_relative "./anime_finder/version"
+require_relative "./anime_finder/CLI"
+
+require 'pry'
 
 module AnimeFinder
   class Error < StandardError; end
