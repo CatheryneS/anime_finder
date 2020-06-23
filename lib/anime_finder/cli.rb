9cd09fc397ab93397fb7e0f096ea3779a94335e8
Catheryne Small <catheryn.small@gmail.com>
2020-06-23 05:02:04 +0000

    working genre list

--- lib/anime_finder/cli.rb
+++ lib/anime_finder/cli.rb
@@ -1,6 +1,8 @@
 class AnimeFinder::CLI 
   def call
     puts "Let's find you an anime to watch!"
+    genres
+    list_genres
     # show catagorizes
     # ask which on they want to look into 
     # provide a list of matching anime
@@ -8,8 +10,13 @@ class AnimeFinder::CLI
     # anime summary is given 
   end
   
-  def catagorizes
-    
+  def genres
+    #gets genres from scraper
+    @genres = ["action", "romance", "slice of life"] # test data 
+  end
+  
+  def list_genres
+    @genres.each.with_index(1) {|g, i| puts "#{i}. #{g}"}
   end
   
   def anime_list
