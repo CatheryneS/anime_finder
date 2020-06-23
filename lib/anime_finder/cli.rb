4cc15e6d36304e31dc7fac66e2c350147c3a2afd
Catheryne Small <catheryn.small@gmail.com>
2020-06-23 21:22:06 +0000

    CLI is now accpeting user input

--- lib/anime_finder/cli.rb
+++ lib/anime_finder/cli.rb
@@ -1,15 +1,29 @@
 class AnimeFinder::CLI 
+  
   def call
-    puts "Let's find you an anime to watch!"
-    genres
-    list_genres
-    anime
-    list_of_animes
+    input = ""
+    
+    until input == "exit"
+    puts "\nLet's find you an anime to watch!\n"
+    puts "\nTo start with a genre, enter 'genres'.\n"
+    input = gets.chomp
+    # list_genres
+    # list_of_animes
     # show catagorizes
     # ask which on they want to look into 
     # provide a list of matching anime
     # user picks an anime 
-    # anime summary is given 
+    # anime summary is given
+    
+    case input
+      when "genres"
+        list_genres
+      when "exit"
+        puts "Enjoy your binge watching! See you in 72 hours."
+      else 
+        puts "\nI don't get it. Please try again.\n"
+      end
+    end
   end
   
   def genres
@@ -18,7 +32,16 @@ class AnimeFinder::CLI
   end
   
   def list_genres
-    @genres.each.with_index(1) {|g, i| puts "#{i}. #{g}"}
+    puts "\nEnter the number of the genre you're interested in?\n"
+    genres.each.with_index(1) {|g, i| puts "#{i}. #{g}"}
+    input = gets.chomp
+    
+      # case input
+      #   when input == index 
+      #     list_of_animes
+      #   else
+      #     "\nPlease use a number of 1-5.\n"
+      #   end
   end
   
   def anime
@@ -27,10 +50,10 @@ class AnimeFinder::CLI
   end
   
   def list_of_animes
-    @anime.each.with_index(1) {|g, i| puts "#{i}. #{g}"}
+    anime.each.with_index(1) {|g, i| puts "#{i}. #{g}"}
   end
   
   def summary
-    
+    #displays the description of the show
   end
 end
--- lib/environment.rb
+++ lib/environment.rb
@@ -2,6 +2,8 @@ require_relative "./anime_finder/version"
 require_relative "./anime_finder/cli"
 
 require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
 module AnimeFinder
   class Error < StandardError; end
