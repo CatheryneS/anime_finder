8f3fd1b4e8e8c10e98ce90ea164f76d048986d70
Catheryne Small <catheryn.small@gmail.com>
2020-06-23 03:04:09 +0000

    adding gems

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
 
+gem 'pry', :group => :development
 
+gem 'nokogiri'
--- lib/anime_finder/CLI.rb
+++ lib/anime_finder/CLI.rb
@@ -1,5 +1,6 @@
 class AnimeFinder::CLI 
   def call
     puts "Let's find you an anime to watch!"
+    binding.pry 
   end
 end
