0a59b7693f4365b7cfb883ddabba5268b66119d5
Catheryne Small <catheryn.small@gmail.com>
2020-06-27 06:26:14 +0000

    scraping links for all detail pages

--- lib/anime_finder/site_scraper.rb
+++ lib/anime_finder/site_scraper.rb
@@ -14,16 +14,25 @@ class AnimeFinder::Scraper
   end
   
   def self.get_anime_list
-     genre_url = "https://aniwatcher.com/anime-list"
+    genre_url = "https://aniwatcher.com/anime-list"
     
     page = Nokogiri::HTML(open(genre_url))
-    binding.pry
-    hyperlinks = page.css('div.column').children.css('li').css('a').attribute('href').value
+    animes = page.css
+    urls = page.css('div.column').children.css('li').css('a').map {|link| link.attribute('href').value} #returns an array of links
+    
+    links = []
+    
+    urls.each do |link|
+      url = "https://aniwatcher.com/series/" + link
+      links << url
+    end
+    scrape_info_page(links)
   end
   
-  def scrape_info_page
+  def scrape_info_page(links)
     # scrapes for anime details (title, genre, episodes, year)
     #create a hash for each new anime
+    
     
   end
 end
